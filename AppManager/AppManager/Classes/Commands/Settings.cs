3b079688ec46abb39b50c92343d98e58718cf964
hummerd_cp <hummerd@mail.ru>
2009-01-08 21:20:04 +0000

    Added button in settings window for opening app data folder

--- AppManager/AppManager/Classes/Commands/Settings.cs
+++ AppManager/AppManager/Classes/Commands/Settings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.IO;
-using System.Reflection;
-using WinSh = IWshRuntimeLibrary;
-
+﻿
 
 namespace AppManager.Commands
 {
@@ -22,41 +16,8 @@ namespace AppManager.Commands
 
 		public override void Execute(object parameter)
 		{
-			Windows.Settings wndSett = new Windows.Settings();
-			wndSett.ChkAutoStart.IsChecked = File.Exists(GetStartUpFile());
-
-			if (wndSett.ShowDialog() ?? false)
-			{
-				SetStartUp(wndSett.ChkAutoStart.IsChecked ?? false);
-			}
-		}
-
-
-		protected void SetStartUp(bool startUp)
-		{
-			string path = GetStartUpFile();
-			bool scExsist = File.Exists(path);
-
-			if (startUp)
-			{
-				WinSh.WshShellClass shell = new WinSh.WshShellClass();
-				WinSh.IWshShortcut lnk;
-
-				lnk = (WinSh.IWshShortcut)shell.CreateShortcut(path);
-				lnk.TargetPath = Assembly.GetExecutingAssembly().Location;
-				lnk.Save();
-			}
-			else
-			{
-				if (scExsist)
-					File.Delete(path);
-			}
-		}
-
-		protected string GetStartUpFile()
-		{
-			string path = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
-			return Path.Combine(path, "AppManager.lnk");
+			Windows.Settings wndSett = new Windows.Settings(_WorkItem);
+			wndSett.ShowDialog();
 		}
 	}
 }
