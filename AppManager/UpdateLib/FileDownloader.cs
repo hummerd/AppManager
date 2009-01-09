30333a7792dea64fd332ca29f95b06d2ae00815b
hummerd_cp <SND\hummerd_cp>
2009-01-09 22:31:25 +0000

    Start working on self update

---
+++ AppManager/UpdateLib/FileDownloader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Policy;
+
+
+namespace UpdateLib
+{
+    public class FileDownloader
+    {
+        public event EventHandler DownloadFileStarted;
+        public event EventHandler DownloadFileCompleted;
+        public event EventHandler DownloadFileSetCompleted;
+        //public event EventHandler DownloadFileCompleted;
+
+
+        public void DownloadFileSetAsync(IEnumerable<Uri> fileLocation)
+        {
+
+        }
+
+        public void DownloadFileAsync(Uri fileLocation)
+        {
+
+        }
+    }
+}
