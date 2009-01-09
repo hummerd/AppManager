917cdbab42a3382cbd7c24ee5e06a7ce97c1ba5c
hummerd_cp <SND\hummerd_cp>
2009-01-09 22:43:47 +0000

    Continue working on self updating

---
+++ AppManager/UpdateLib/FileDownloader/IFileDownloader.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpdateLib.FileDownloader
+{
+    public interface IFileDownloader
+    {
+    }
+}
---
+++ AppManager/UpdateLib/SelfUpdate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace UpdateLib
+{
+    public class SelfUpdate
+    {
+    }
+}
