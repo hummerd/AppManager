5f8352c73788f212103f743a865954139467ba4a
hummerd_cp <SND\hummerd_cp>
2009-02-07 21:40:50 +0000

    1. App context menu now works 2. Changed some styles for correct appearance 3. Added style for app context menu

---
+++ AppManager/AppManager/Windows/InputBox.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppManager.Windows
+{
+	/// <summary>
+	/// Interaction logic for InputBox.xaml
+	/// </summary>
+	public partial class InputBox : Window
+	{
+		public InputBox(string caption)
+		{
+			InitializeComponent();
+			Title = caption;
+		}
+
+
+		public string InputText
+		{ 
+			get
+			{
+				return TxtInput.Text;
+			}
+			set
+			{
+				TxtInput.Text = value;
+			}
+		}
+
+
+		private void BtnOk_Click(object sender, RoutedEventArgs e)
+		{
+			DialogResult = true;
+		}
+
+		private void BtnCancel_Click(object sender, RoutedEventArgs e)
+		{
+			DialogResult = false;
+		}
+	}
+}
