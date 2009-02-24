7d8ca346d168fff341b9a6fed07f331c1c3811f7
hummerd_cp <hummerd@mail.ru>
2009-02-24 15:41:53 +0000

    Added shortcut key F1

---
+++ AppManager/AppManager/Classes/Commands/Help.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AppManager.Windows;
+
+
+namespace AppManager.Commands
+{
+	public class Help : CommandBase
+	{
+		protected HelpBox _HelpWnd;
+
+
+		public Help(MainWorkItem workItem)
+			: base(workItem)
+		{ ; }
+
+
+		public override bool CanExecute(object parameter)
+		{
+			return true;
+		}
+
+		public override void Execute(object parameter)
+		{
+			if (_HelpWnd != null)
+			{
+				_HelpWnd.Activate();
+				return;
+			}
+
+			_HelpWnd = new HelpBox(_WorkItem, (bool)parameter);
+			_HelpWnd.Owner = _WorkItem.MainWindow;
+			_HelpWnd.Closed += HelpWnd_Closed;
+			_HelpWnd.Show();
+		}
+
+
+		private void HelpWnd_Closed(object sender, EventArgs e)
+		{
+			_HelpWnd = null;
+		}
+	}
+}
