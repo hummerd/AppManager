4ff0f314261fdbe78745ce7c2e0dfdcc85a19735
hummerd_cp <hummerd@mail.ru>
2009-03-01 21:14:40 +0000

    Working on app type auto generation

--- AppManager/CommonLib/Resizer.cs
+++ AppManager/CommonLib/Resizer.cs
@@ -4,6 +4,8 @@ using System.Text;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 
 namespace CommonLib
@@ -11,15 +13,26 @@ namespace CommonLib
 	public class Resizer
 	{
 		protected Grid					_Target;
-		protected FrameworkElement _Resizer;
+		protected Shape				_Resizer;
+		protected UIElement			_TargetChild;
+		protected int					_GridRow;
 		protected Point				_InGridPos;
 		protected bool					_DoResize = false;
 
 
-		public Resizer(FrameworkElement resizer)
+		public Resizer(Shape resizer)
 		{
 			_Resizer = resizer;
 			_Target = UIHelper.FindAncestorOrSelf<Grid>(_Resizer);
+
+			foreach (UIElement item in _Target.Children)
+				if (_Resizer.FindCommonVisualAncestor(item) != null)
+				{
+					_TargetChild = item;
+					break;
+				}
+			
+			_GridRow = Grid.GetRow(_TargetChild);
 
 			resizer.MouseDown += new MouseButtonEventHandler(resizer_MouseDown);
 			resizer.MouseUp += new MouseButtonEventHandler(resizer_MouseUp);
@@ -30,8 +43,23 @@ namespace CommonLib
 		private void resizer_MouseMove(object sender, MouseEventArgs e)
 		{
 			if (_DoResize)
-			{ 
-				
+			{
+				double newHeight = e.GetPosition(_Target).Y;
+				double totalRelHeight = 0.0;
+				double totalHeight = 0.0;
+
+				for (int i = 0; i <= _GridRow; i++)
+				{
+					totalHeight += _Target.RowDefinitions[i].ActualHeight;
+					totalRelHeight += _Target.RowDefinitions[i].Height.Value;
+				}
+
+				double newRelHeight = newHeight * totalRelHeight / totalHeight;
+				for (int i = 0; i <= _GridRow; i++)
+				{
+					double p = _Target.RowDefinitions[i].Height.Value / totalRelHeight;
+					_Target.RowDefinitions[i].Height = new GridLength(p * newRelHeight, GridUnitType.Star);
+				}
 			}
 		}
 		
@@ -40,12 +68,14 @@ namespace CommonLib
 			_Resizer.CaptureMouse();
 			_InGridPos = e.GetPosition(_Target);
 			_DoResize = true;
+			_Resizer.Fill = Brushes.Red;
 		}
 
 		private void resizer_MouseUp(object sender, MouseButtonEventArgs e)
 		{
 			_Resizer.ReleaseMouseCapture();
 			_DoResize = false;
+			_Resizer.Fill = Brushes.Transparent;
 		}
 	}
 }
