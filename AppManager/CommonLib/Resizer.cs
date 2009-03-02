e0ca38055d551a0c1aa421bc2638b922354561b6
hummerd_cp <SND\hummerd_cp>
2009-03-02 20:57:43 +0000

    Still refactoring

--- AppManager/CommonLib/Resizer.cs
+++ AppManager/CommonLib/Resizer.cs
@@ -42,9 +42,34 @@ namespace CommonLib
 			
 			_GridRow = Grid.GetRow(_TargetChild);
 
+			if (_GridRow == _Target.RowDefinitions.Count - 1)
+			{
+				var cont = VisualTreeHelper.GetParent(_Resizer) as Panel;
+				cont.Children.Remove(resizer);
+			}
+
 			resizer.MouseDown += new MouseButtonEventHandler(resizer_MouseDown);
 			resizer.MouseUp += new MouseButtonEventHandler(resizer_MouseUp);
 			resizer.MouseMove += new MouseEventHandler(resizer_MouseMove);
+		}
+
+		protected void SetRelHeight(int sRow, int eRow, double newHeight)
+		{
+			double totalRelHeight = 0.0;
+			double totalHeight = 0.0;
+
+			for (int i = sRow; i <= eRow; i++)
+			{
+				totalHeight += _Target.RowDefinitions[i].ActualHeight;
+				totalRelHeight += _Target.RowDefinitions[i].Height.Value;
+			}
+
+			double newRelHeight = newHeight * totalRelHeight / totalHeight;
+			for (int i = sRow; i <= eRow; i++)
+			{
+				double p = _Target.RowDefinitions[i].Height.Value / totalRelHeight;
+				_Target.RowDefinitions[i].Height = new GridLength(p * newRelHeight, GridUnitType.Star);
+			}
 		}
 
 
@@ -52,22 +77,17 @@ namespace CommonLib
 		{
 			if (_DoResize)
 			{
-				double newHeight = e.GetPosition(_Target).Y;
-				double totalRelHeight = 0.0;
-				double totalHeight = 0.0;
+				SetRelHeight(
+					0, 
+					_GridRow,
+					e.GetPosition(_Target).Y);
 
-				for (int i = 0; i <= _GridRow; i++)
-				{
-					totalHeight += _Target.RowDefinitions[i].ActualHeight;
-					totalRelHeight += _Target.RowDefinitions[i].Height.Value;
-				}
+				SetRelHeight(
+					_GridRow + 1,
+					_Target.RowDefinitions.Count - 1,
+					_Target.ActualHeight - e.GetPosition(_Target).Y);
 
-				double newRelHeight = newHeight * totalRelHeight / totalHeight;
-				for (int i = 0; i <= _GridRow; i++)
-				{
-					double p = _Target.RowDefinitions[i].Height.Value / totalRelHeight;
-					_Target.RowDefinitions[i].Height = new GridLength(p * newRelHeight, GridUnitType.Star);
-				}
+				e.Handled = true;
 			}
 		}
 		
