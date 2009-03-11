9db0427f3de97c68031800c5e22dc02121360fe1
hummerd_cp <SND\hummerd_cp>
2009-03-11 09:31:09 +0000

    Added kernel32 class

---
+++ AppManager/CommonLib/PInvoke/Kernel32.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+
+namespace CommonLib.PInvoke
+{
+	class Kernel32
+	{
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+		public static extern int GetCurrentThreadId();
+	}
+}
