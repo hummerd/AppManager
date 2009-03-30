b05634da817b661a0a7288451f1b451ced37921f
hummerd_cp <SND\hummerd_cp>
2009-03-30 16:24:04 +0000

    Automatic version creation when building release

--- AppManager/VersionBuilder/App.xaml.cs
+++ AppManager/VersionBuilder/App.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Windows;
+using CommonLib.Windows;
+
 
 namespace VersionBuilder
 {
@@ -11,5 +13,36 @@ namespace VersionBuilder
 	/// </summary>
 	public partial class App : Application
 	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			if (e.Args.Length > 0)
+			{
+				try
+				{
+					VersionFactory vf = new VersionFactory();
+					vf.CreateVersion(
+						e.Args[0].Substring(1, e.Args[0].Length - 1),
+						new Version(e.Args[1].Substring(1, e.Args[1].Length - 1)),
+						e.Args[2].Substring(1, e.Args[2].Length - 1),
+						e.Args[3].Substring(1, e.Args[3].Length - 1)
+						);
+
+					Shutdown();
+				}
+				catch (Exception exc)
+				{
+					ErrorBox.Show("Error", exc);
+					Shutdown(-1);
+				}
+			}
+			else
+			{
+				MainWindow mw = new MainWindow();
+				MainWindow = mw;
+				mw.Show();
+			}
+		}
 	}
 }
