d4c0f29162d89435a1eea8adacd472d6a36b5a69
hummerd_cp <hummerd@mail.ru>
2009-06-23 09:14:14 +0000

    Added feedback for dropping files upon apps with holding alt key

---
+++ AppManager/AppManager/Classes/DragDrop/FileAppDropHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using DragDropLib;
+
+
+namespace AppManager.DragDrop
+{
+	public class FileAppDropHandler : FileDropHandler
+	{
+		public override System.Windows.DragDropEffects SupportDataFormat(FrameworkElement element, DragEventArgs dragData)
+		{
+			if ((dragData.KeyStates & DragDropKeyStates.AltKey) == DragDropKeyStates.AltKey)
+			{
+				var input = (element as ItemsControl).InputHitTest(dragData.GetPosition(element)) as FrameworkElement;
+				if (input != null)
+				{
+					var appInfo = input.DataContext as AppInfo;
+					if (appInfo != null)
+						return DragDropEffects.Link;
+				}
+			}
+
+			return base.SupportDataFormat(element, dragData);
+		}
+	}
+}
--- AppManager/AppManager/Controls/ButtonList.xaml.cs
+++ AppManager/AppManager/Controls/ButtonList.xaml.cs
@@ -2,7 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
+using AppManager.DragDrop;
 using CommonLib;
 
 
