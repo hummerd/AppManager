dbc26f0164f4d6df42159ba5ef38c773dc5ed136
hummerd_cp <hummerd@mail.ru>
2010-11-18 08:17:46 +0000

    proxy now configured in app.config

--- AppManager/UpdateLib/WebUpdate/WebFileDownloader.cs
+++ AppManager/UpdateLib/WebUpdate/WebFileDownloader.cs
@@ -8,23 +8,7 @@ namespace UpdateLib.WebUpdate
 {
 	public class WebFileDownloader : FileDownloaderBase
 	{
-		protected ICredentials	_Credentials;
-		protected bool				_PreAuthenticate;
-		protected string			_UserAgent = null;
 		protected WebResponse	_DownloadResponse;
-
-
-		public WebFileDownloader()
-		{
-			_Credentials = null;
-			_PreAuthenticate = true;
-
-			//WebProxy proxyObject = new WebProxy(WebProxy.GetDefaultProxy().Address);
-			//proxyObject.UseDefaultCredentials = true;
-
-			//_Proxy = proxyObject;
-			_UserAgent = null;
-		}
 
 
 		protected override Stream GetFileStream(Uri location)
@@ -53,21 +37,10 @@ namespace UpdateLib.WebUpdate
 			if (webRequest != null)
 			{
 				webRequest.Timeout = 10000;
-				request.Credentials = _Credentials;
-				request.PreAuthenticate = _PreAuthenticate;
-
-				// The default for the HttpWebRequest is for this value to
-				// be null, so only do this if the UserAgent value is not
-				// null or empty.
-				if (!String.IsNullOrEmpty(_UserAgent))
-				{
-					webRequest.UserAgent = _UserAgent;
-				}
+				//request.Credentials = _Credentials;
+				request.PreAuthenticate = true;
 			}
 
-			var prx = new WebProxy(WebRequest.DefaultWebProxy.GetProxy(url));
-			prx.UseDefaultCredentials = true;
-			request.Proxy = prx;
 			return request;
 		}
 	}
