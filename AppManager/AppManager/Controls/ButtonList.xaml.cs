5a8ae81c7eb823e9c87d5438381fccedeb02a539
hummerd_cp <hummerd@mail.ru>
2011-04-14 08:02:45 +0000

    Ui improvement

--- AppManager/AppManager/Controls/ButtonList.xaml.cs
+++ AppManager/AppManager/Controls/ButtonList.xaml.cs
@@ -139,21 +139,4 @@ namespace AppManager
 				ButtonClicked(this, new ValueEventArgs<object>(ib.DataContext));
 		}
 	}
-
-	public class WidthCorrector : IValueConverter
-	{
-		#region IValueConverter Members
-
-		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			return (double)value - double.Parse(parameter.ToString(), CultureInfo.InvariantCulture);
-		}
-
-		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			throw new NotImplementedException();
-		}
-
-		#endregion
-	}
 }
