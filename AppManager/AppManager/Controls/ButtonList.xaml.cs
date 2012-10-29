e4c47391e7b4889d60210124c8b03deb3515285a
hummerd_cp <SND\hummerd_cp>
2012-10-29 21:30:47 +0000

    Fixed drag animation

--- AppManager/AppManager/Controls/ButtonList.xaml.cs
+++ AppManager/AppManager/Controls/ButtonList.xaml.cs
@@ -15,6 +15,7 @@ using System.Collections.Generic;
 using System.Collections;
 using System.Windows.Controls.Primitives;
 using AppManager.Classes.ViewModel;
+using System.Windows.Threading;
 
 
 namespace AppManager
@@ -33,6 +34,8 @@ namespace AppManager
 		protected FrameworkElement _LastMoved = null;
 		protected Rect _LastRect;
 		protected bool _IsAppTitleVisible;
+        protected DispatcherTimer _HoverTimer;
+        protected DateTime _LastDragOver;
 
 
 		public ButtonList()
@@ -43,9 +46,13 @@ namespace AppManager
 
 			_DragHelper = new ButtonListDrag<AppInfo>(this);
 			_DragHelper.DragOver += (s, e) => OnDragHelperDragOver(e);
-			_DragHelper.DragLeave += (s, e) => ResetLastMove(e, false);
-			_DragHelper.DragDroped += (s, e) => ResetLastMove(e, true);
+			//_DragHelper.DragLeave += (s, e) => ResetLastMove(e, false);
+			_DragHelper.DragDroped += (s, e) => ResetLastMove();
 			_DragHelper.NeedTargetObject += (s, e) => e.Value = GetLastItems();
+
+            _HoverTimer = new DispatcherTimer();
+            _HoverTimer.Interval = new TimeSpan(0, 0, 0, 0, 500);
+            _HoverTimer.Tick += (s, e) => CheckDrag();
 		}
 
 
@@ -73,9 +80,38 @@ namespace AppManager
 		}
 
 
+        protected void CheckDrag()
+        {
+            var pt = User32.GetCursorPos(this);
+            //System.Diagnostics.Debug.WriteLine("timer " + pt);
+            bool ended = (DateTime.Now - _LastDragOver).TotalMilliseconds > 400;
+            
+            if (!_LastRect.IsEmpty && _LastRect.Contains(pt) && !ended)
+                return;
+                        
+			var input = InputHitTest(pt) as FrameworkElement;
+            if (input == null || ended)
+            {
+                ResetLastMove();
+                _HoverTimer.Stop();
+            }
+            else
+            {
+                var ib = UIHelper.FindAncestorOrSelf<AppButton>(input, null);
+                if (ib == null)
+                {
+                    ResetLastMove();
+                    _HoverTimer.Stop();
+                }
+            }
+        }
+
 		protected void OnDragHelperDragOver(DragEventArgs e)
 		{
-			Point pt = e.GetPosition(this);
+            _LastDragOver = DateTime.Now;
+			var pt = e.GetPosition(this);
+            //System.Diagnostics.Debug.WriteLine("rect set " + pt);
+
 			if (!_LastRect.IsEmpty && _LastRect.Contains(pt))
 				return;
 
@@ -85,39 +121,26 @@ namespace AppManager
 				var ib = UIHelper.FindAncestorOrSelf<AppButton>(input, null);
 				if (ib != null && _LastMoved != ib)
 				{
-					ResetLastMove(e, true);
+					ResetLastMove();
 					_LastMoved = ib;
 					_LastRect = new Rect(ib.TranslatePoint(new Point(0, 0), this), ib.RenderSize);
-					//Debug.WriteLine("rect set " + _LastRect);
+					System.Diagnostics.Debug.WriteLine("rect set " + _LastRect);
 					new MoveAnimation(ib);
+                    _HoverTimer.Start();
 				}
 				else if (ib == null)
-					ResetLastMove(e, true);
+					ResetLastMove();
 			}
 		}
 
-		protected void ResetLastMove(DragEventArgs e, bool anyWay)
+		protected void ResetLastMove()
 		{
-			//Debug.WriteLine("reset " + e.Source);
-			//Debug.WriteLine("reset " + e.OriginalSource);
-
-			if (e.GetPosition((IInputElement)e.OriginalSource) == new Point(0, 0))
-				anyWay = true;
-
-			Point pt = User32.GetCursorPos(this);
-			var wnd = UIHelper.FindAncestorOrSelf<Window>(this, null);
-			var wndi = wnd.InputHitTest(User32.GetCursorPos(wnd));
-			var input = InputHitTest(pt) as FrameworkElement;
-
-			if (wndi == input && !anyWay && !_LastRect.IsEmpty && _LastRect.Contains(pt))
-				return;
-			
 			if (_LastMoved != null)
 				_LastMoved.RenderTransform.BeginAnimation(TranslateTransform.XProperty, null);
 
 			_LastRect = Rect.Empty;
 			_LastMoved = null;
-		}
+        }
 
 		protected object GetLastItems()
 		{
